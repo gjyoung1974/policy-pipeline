ab9cb44a6ebfa99ce3f23711780805e5b591dcae
Gordon Young <gyoung@Gordons-MacBook-Pro.local>
2019-01-26 11:58:09 -0700

    modified:   build/app/render.sh

--- build/app/render.sh
+++ build/app/render.sh
@@ -2,9 +2,8 @@
 echo "Running render actions"
 cd /source/
 git clone https://github.com/${gh_user}/${gh_repo}.git
-cd ${gh_repo}
-comply build
 cd /source/${gh_repo}
+comply build
 git config --global user.email ${gh_email}
 git config --global user.name ${gh_user}
 git remote set-url origin https://${gh_user}:${gh_token}@github.com/${gh_user}/${gh_repo}.git
